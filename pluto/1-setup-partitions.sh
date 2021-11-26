e80ec9b457e0f6390992d7e3f50e7da9a9378483
Stefan Svellingen <stefan@svellingen.org>
2021-11-26 13:14:14 +0100

    some fixes and remove swap partition as we have moved to a swapfile instead

--- pluto/1-setup-partitions.sh
+++ pluto/1-setup-partitions.sh
@@ -6,15 +6,14 @@ echo "---- setup partitions ---"
 
 sgdisk --clear \
          --new=1:0:+500MiB --typecode=1:ef00 --change-name=1:EFI \
-         --new=2:0:+16GiB  --typecode=2:8200 --change-name=2:swap \
-         --new=3:0:0       --typecode=3:8e00 --change-name=3:crypt /dev/sda
+         --new=2:0:0       --typecode=3:8e00 --change-name=3:crypt /dev/sda
 
 echo "type passphrase"
 read -s pass
 echo "retype passphrase"
-read -s pass2
+read -s passCheck
 
-if [ "$pass" = "$pass2" ]; then
+if [ "$pass" = "$passCheck" ]; then
        	echo "password match, continue"
     else
     echo "passwords not equal, exiting"
@@ -31,7 +30,7 @@ echo -n $pass | cryptsetup open /dev/disk/by-partlabel/crypt lvm
 pvcreate --dataalignment 1m /dev/mapper/lvm
 vgcreate volgroup0 /dev/mapper/lvm
 lvcreate -L 50GB volgroup0 -n lv_root
-lvcreate -l 100%FREE volgroup1 -n lv_home
+lvcreate -l 100%FREE volgroup0 -n lv_home
 
 modprobe dm_mod
 vgscan
@@ -43,19 +42,11 @@ mount /dev/volgroup1/lv_root /mnt
 mkdir /mnt/boot
 mount /dev/disk/by-partlabel/EFI /mnt/boot
 
-mkfs.ext4 /dev/volgroup1/lv_home
+mkfs.ext4 /dev/volgroup0/lv_home
 mkdir /mnt/home
 mount /dev/volgroup1/lv_home /mnt/home
 
 mkdir /mnt/etc
 genfstab -U -p /mnt >> /mnt/etc/fstab
 cat /mnt/etc/fstab
-
-
-# Bring Up Encrypted Swap
-#cryptsetup open --type plain --key-file /dev/urandom /dev/disk/by-partlabel/cryptswap swap
-#mkswap -L swap /dev/mapper/swap
-#swapon -L swap
-
-
 
