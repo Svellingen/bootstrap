21e12bd6f32c7e55f3b2ce1e6259d847d2abb24a
Stefan Svellingen <stefan@svellingen.org>
2021-12-04 15:41:33 +0100

    adding run file for carbon laptop specific issues

---
+++ gorilla/5-carbon-specific.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# https://wiki.archlinux.org/title/Lenovo_ThinkPad_X1_Carbon_(Gen_7)
+# https://wiki.archlinux.org/title/Lenovo_ThinkPad_X1_Carbon_(Gen_8)
+
+# https://pcsupport.lenovo.com/us/en/products/laptops-and-netbooks/thinkpad-x-series-laptops/thinkpad-x1-carbon-7th-gen-type-20qd-20qe/downloads/driver-list
+
+# https://wiki.archlinux.org/title/fwupd
+sudo pacman -Syy fwupd
+
