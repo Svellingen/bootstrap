9da14c3200fd4bc09cd2c128ca6c8d77845fd21e
Stefan Svellingen <stefan@svellingen.org>
2021-12-04 14:18:41 +0100

    sda2 -> nvme0n1p2

--- gorilla/4-bootloader.sh
+++ gorilla/4-bootloader.sh
@@ -8,7 +8,7 @@ mount /dev/nvme0n1p1 /boot/efi
 grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck
 cp /usr/share/locale/en\@quot/LC_MESSAGES/grub.mo /boot/grub//locale/en.mo
 sed -i '/GRUB_ENABLE_CRYPTODISK/s/^#//g' /etc/default/grub
-sed -i 's/loglevel=3/cryptdevice=\/dev\/sda2:volgroup:0 loglevel=3/g' /etc/default/grub
+sed -i 's/loglevel=3/cryptdevice=\/dev\/nvme0n1p2:volgroup:0 loglevel=3/g' /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
 
 echo "exit chroot, umount -a and reboot"
